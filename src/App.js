058d729ac3402caf23628d852ca12ab773a9844f
Mohammad majed alshish <eng.mohammed.majed@gmail.com>
2022-05-17 19:38:12 +0300

    not yet

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
-import FavList from './components/FavList/FavList';
+
 
 function App() {
   return (
     <>
-    <Navbar/>
+
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/favList" element={<FavList/>} />
       </Routes>
     </>
   );
